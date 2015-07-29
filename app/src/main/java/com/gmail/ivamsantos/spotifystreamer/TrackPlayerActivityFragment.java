0a386c119d02a69ab6833ec12eff1fd2f935b4da
iluz <iluz@ciandt.com>
2015-07-28 23:37:17 -0300

    Start implementing actual player functionality

--- app/src/main/java/com/gmail/ivamsantos/spotifystreamer/TrackPlayerActivityFragment.java
+++ app/src/main/java/com/gmail/ivamsantos/spotifystreamer/TrackPlayerActivityFragment.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -32,13 +31,16 @@ public class TrackPlayerActivityFragment extends Fragment {
     public static final String LOG_TAG = TrackPlayerActivityFragment.class.getSimpleName();
     public static final int TRACK_INDEX_UNAVAILABLE = -1;
 
+    boolean isPlaying = false;
+    private View mRootView;
     private String mArtistName;
     private Track mCurrentTrack;
+    private Integer mCurrentTrackIndex;
     private ArrayList<Track> mTracks;
 
+    private ActionBar mActionBar;
 
-    View mRootView;
-    boolean isPlaying = false;
+    private MediaPlayer mMediaPlayer;
 
     public TrackPlayerActivityFragment() {
     }
@@ -50,7 +52,7 @@ public class TrackPlayerActivityFragment extends Fragment {
 
         loadValuesFromIntent();
         setupActionBar();
-        bindLayoutComponentsValues();
+        setLayoutComponentsValues();
         setupPlayerControlButtons();
 
         return mRootView;
@@ -62,19 +64,19 @@ public class TrackPlayerActivityFragment extends Fragment {
         mTracks = intent.getParcelableArrayListExtra(getString(R.string.extra_tracks));
 
         // FIXME: Handle invalid index
-        int trackIndex = intent.getIntExtra(getString(R.string.extra_track_index), TRACK_INDEX_UNAVAILABLE);
-        mCurrentTrack = mTracks.get(trackIndex);
+        mCurrentTrackIndex = intent.getIntExtra(getString(R.string.extra_track_index), TRACK_INDEX_UNAVAILABLE);
+        mCurrentTrack = mTracks.get(mCurrentTrackIndex);
     }
 
     private void setupActionBar() {
         // http://www.slideshare.net/cbeyls/android-32084115 (slide 13)
-        ActionBar actionBar = ((ActionBarActivity) getActivity()).getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setTitle(mArtistName);
-        actionBar.setSubtitle(mCurrentTrack.name);
+        mActionBar = ((ActionBarActivity) getActivity()).getSupportActionBar();
+        mActionBar.setDisplayHomeAsUpEnabled(true);
+        mActionBar.setTitle(mArtistName);
+        mActionBar.setSubtitle(mCurrentTrack.name);
     }
 
-    private void bindLayoutComponentsValues() {
+    private void setLayoutComponentsValues() {
         TextView artistNameTextView = (TextView) mRootView.findViewById(R.id.trackPlayerArtistName);
         TextView albumNameTextView = (TextView) mRootView.findViewById(R.id.trackPlayerAlbumName);
         TextView trackNameTextView = (TextView) mRootView.findViewById(R.id.trackPlayerTrackName);
@@ -114,7 +116,7 @@ public class TrackPlayerActivityFragment extends Fragment {
         previousImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getActivity(), "Moving to previous track.", Toast.LENGTH_SHORT).show();
+                previous();
             }
         });
     }
@@ -129,24 +131,109 @@ public class TrackPlayerActivityFragment extends Fragment {
                 if (isPlaying) {
                     // http://stackoverflow.com/a/8354606
                     playImageButton.setImageResource(android.R.drawable.ic_media_pause);
-                    Toast.makeText(getActivity(), "Playing track.", Toast.LENGTH_SHORT).show();
+                    play();
                 } else {
                     playImageButton.setImageResource(android.R.drawable.ic_media_play);
-                    Toast.makeText(getActivity(), "Paused track.", Toast.LENGTH_SHORT).show();
-
-                    String url = "http://........"; // your URL here
-                    MediaPlayer mediaPlayer = new MediaPlayer();
-                    mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-                    try {
-                        mediaPlayer.setDataSource(url);
-                        mediaPlayer.prepare();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    mediaPlayer.start();
+                    pause();
                 }
             }
         });
+    }
+
+    private void play() {
+        String url = mCurrentTrack.preview_url;
+
+        if (mMediaPlayer == null) {
+            mMediaPlayer = new MediaPlayer();
+
+            mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            mMediaPlayer.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+                @Override
+                public boolean onError(MediaPlayer mp, int what, int extra) {
+                    Toast.makeText(getActivity(), "An error occurred while playing the track.", Toast.LENGTH_SHORT).show();
+                    mMediaPlayer.reset();
+                    // ... react appropriately ...
+                    // The MediaPlayer has moved to the Error state, must be reset!
+                    return false;
+                }
+            });
+
+            mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    mMediaPlayer.start();
+                }
+            });
+
+            mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    playNext();
+                }
+            });
+        }
+
+        mMediaPlayer.reset();
+        try {
+            mMediaPlayer.setDataSource(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        mMediaPlayer.prepareAsync();
+    }
+
+    private void playNext() {
+        foward();
+        play();
+    }
+
+    private void pause() {
+        Toast.makeText(getActivity(), "Paused track.", Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * Move the player to the previous track. If currently on the last track, moves to the
+     * last one.
+     */
+    private void previous() {
+        boolean isOnFirstTrack = (mCurrentTrackIndex == 0);
+        if (isOnFirstTrack) {
+            int lastIndex = (mTracks.size() - 1);
+            mCurrentTrackIndex = lastIndex;
+        } else {
+            mCurrentTrackIndex--;
+        }
+
+        updateCurrentTrack();
+    }
+
+    /**
+     * Move the player to the next track. If currently on the last track, moves to the first one.
+     */
+    private void foward() {
+        boolean isOnLastTrack = mCurrentTrackIndex == (mTracks.size() - 1);
+        if (isOnLastTrack) {
+            mCurrentTrackIndex = 0;
+        } else {
+            mCurrentTrackIndex++;
+        }
+
+        updateCurrentTrack();
+    }
+
+    /**
+     * Update screen with the new current track info. If is currently playing, start playing the
+     * new current track.
+     */
+    private void updateCurrentTrack() {
+        mCurrentTrack = mTracks.get(mCurrentTrackIndex);
+        setLayoutComponentsValues();
+        mActionBar.setSubtitle(mCurrentTrack.name);
+
+        if (mMediaPlayer != null && mMediaPlayer.isPlaying()) {
+            play();
+        }
     }
 
     private void setupNextTrackButton() {
@@ -154,8 +241,7 @@ public class TrackPlayerActivityFragment extends Fragment {
         nextImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getActivity(), "Moving to next track.", Toast.LENGTH_SHORT).show();
-                ;
+                foward();
             }
         });
     }
